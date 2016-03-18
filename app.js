a5b694f75183ba5a542e80dfa9a205690282a607
Lucas Sousa <lucasfixa@gmail.com>
2016-03-17 23:41:41 -0300

    Port fix

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.get('/hello', function (req, res) {
   res.send('Started with Hello!!!');
 });
 
-app.listen(8080, function () {
-  console.log('Listening on port 8080!');
+app.listen(port, function () {
+  console.log('Listening on port: ' + port);
 });
