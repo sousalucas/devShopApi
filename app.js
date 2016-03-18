de7b154b4f4ef522c36072f5620f2623c8843ec8
Lucas Sousa <lucasfixa@gmail.com>
2016-03-17 23:39:04 -0300

    Port fix

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ app.get('/hello', function (req, res) {
   res.send('Started with Hello!!!');
 });
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+app.listen(8080, function () {
+  console.log('Listening on port 8080!');
 });
