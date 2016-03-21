5a8c3f2c21071bb53444c3d47e43e80164c25a0d
Lucas Sousa <lucasfixa@gmail.com>
2016-03-21 18:37:07 -0300

    Devs crud endpoints

--- app.js
+++ app.js
@@ -9,6 +9,26 @@ router.get('/healthcheck', function (req, res) {
   res.json({ message: 'OK!' });
 });
 
+router.route('/devs')
+
+    .get(function(req, res) {
+        res.json({ developers: [{ name: 'dev1', age: 25}, { name: 'dev2', age: 35}, { name: 'dev3', age: 28}] });
+    })
+
+    .post(function(req, res) {
+        res.json({ message: 'Dev Saved!' });
+    })
+
+    .delete(function(req, res) {
+        res.json({ message: 'Dev Fired!' });
+    });
+
+router.route('/devs/:dev_id')
+
+    .get(function(req, res) {
+        res.json({ message: 'Dev selected: ' + req.params.dev_id });
+    });
+
 app.use('/api', router);
 
 app.listen(port, function () {
